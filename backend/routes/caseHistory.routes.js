c8e868715f5d2b3986f4d119be1d1557eab291cc
Abdul Waqas <www.waqas7172@gmail.com>
2025-11-06 01:14:57 +0500

    add frontend

--- backend/routes/caseHistory.routes.js
+++ backend/routes/caseHistory.routes.js
@@ -4,7 +4,11 @@ const verifyToken = require("../middlewares/verifyToken");
 const editCaseHistory = require("../controllers/caseHistory/editCaseHistory.controller");
 const router = express.Router();
 
+// Create Case History (only doctor)
 router.post("/create", verifyToken, createCaseHistory);
 
+// Edit Case History (only doctor who created it)
 router.put("/edit/:caseHistoryId", verifyToken, editCaseHistory);
+
 module.exports = router;
+
